327e2916cc006fab6448c16c15e7b0b8fbb970e5
Itzel <itzeljimenez.do@gmail.com>
2017-03-16 10:30:11 +0000

    intento del ejercicio grados

---
+++ ejercicioPractica2/archivo.js
@@ -0,0 +1,13 @@
+ var celsius = document.getElementById('celsius').value;
+ var farenheit = document.getElementById('farenheit').value;
+
+function convertirAfarenheit(){
+
+    if (Convertir)
+        return document.getElementById("celsius").value * 9 / 5 + 32;
+        document.getElementById("farenheit").value = Math.round(x);
+   };
+}
+function convertirAcelsius(farenheit){
+
+}
